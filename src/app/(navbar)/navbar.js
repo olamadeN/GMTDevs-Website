e4f1abe5fee0149b7d9da5eeec5083457e04c557
Gmt Dev <gmtdev@gmail.com>
2025-04-29 15:39:10 +0100

    responsivness about start

--- src/app/(navbar)/navbar.js
+++ src/app/(navbar)/navbar.js
@@ -1,23 +1,49 @@
-import Image from "next/image"
+'use client';
+import { useState } from "react";
+import Image from "next/image";
 import nav from './navbar.module.css';
 import Link from "next/link";
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+    const [servicesOpen, setServicesOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    const toggleServices = () => {
+        setServicesOpen(!servicesOpen);
+    };
+
     return (
         <div>
-            <nav className={`${nav.navbar}`}>
+            <nav className={nav.navbar}>
                 <Image className={nav.logo} src='/images/logo.png' alt='navigation logo' width={100} height={100} />
-                <ol className={nav.navLinks}>
-                    <Link href="/"><li>Home</li></Link> {/* The leading slash is to ensure the route is absoulte to prevent it being interpreted as a relative route by the app router in the case details page */}
+
+                <div className={nav.hamburger} onClick={toggleMenu}>
+                    <div className={nav.bar}></div>
+                    <div className={nav.bar}></div>
+                    <div className={nav.bar}></div>
+                </div>
+
+                <ol className={`${nav.navLinks} ${menuOpen ? nav.showMenu : ''}`}>
+                    <Link href="/"><li>Home</li></Link>
                     <Link href="/about"><li>About</li></Link>
-                    <Link href="/service"><li>Services</li></Link>
+                    <li className={nav.dropdown} onClick={toggleServices}>
+                        Services
+                        <div className={`${nav.dropdownContent} ${servicesOpen ? nav.showSubMenu : ''}`}>
+                            <Link href="/services/design"><li>Design Services</li></Link>
+                            <Link href="/services/development"><li>Development Services</li></Link>
+                        </div>
+                    </li>
                     <Link href="/case"><li>Case Studies</li></Link>
                     <Link href="/"><li>Contacts</li></Link>
-                    <Link href="#"><li>Learn More</li></Link>                                 
+                    <Link href="#"><li>Learn More</li></Link>
                 </ol>
             </nav>
         </div>
-    )
-}
+    );
+};
 
-export default Navbar
+export default Navbar;
