800ea399ca7e5038fdd3753bae19e27635c9d61a
Gmt Dev <gmtdev@gmail.com>
2025-05-14 15:14:20 +0100

    faq adjustment

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,6 +11,20 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/*Google tag (gtag.js) */}
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=G-DCET6HD4LW`}></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.dataLayer = window.dataLayer || [];
+              function gtag(){dataLayer.push(arguments);}
+              gtag('js', new Date());
+              gtag('config', 'G-DCET6HD4LW');
+            `,
+          }}
+        />
+      </head>
       <body>
         <Navbar />
         {children}
