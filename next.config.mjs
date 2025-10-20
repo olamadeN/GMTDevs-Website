3a2b5025a88c95ad6b62dda84d5ee1534e05f4b8
Gmt Dev <gmtdev@gmail.com>
2025-10-20 18:25:50 +0100

    moving to vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
+  /*   output: 'export',
     images: {
         domains: ['your-image-host.com', 'firebasestorage.googleapis.com'], // Add external image sources if needed
         unoptimized: true, // Disable Next.js default optimization since Firebase does not support it
-    },
+    }, */
 };
 
 export default nextConfig;
