e86ebd54ef8789d39c4f2189fa19bab519a56f40
Gmt Dev <gmtdev@gmail.com>
2026-07-10 10:20:37 +0100

    font update

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,33 @@
 import "./globals.css";
+import { Belanosima, DM_Sans, Montserrat, Inter, Poppins} from "next/font/google";
 import Navbar from "./(navbar)/navbar";
 import Footer from "./(footer)/footer";
+
+
+const Belanosima = Belanosima({
+  variable: "--Belanosima",
+  subsets: ["latin"],
+});
+
+const DM_Sans = DM_Sans({
+  variable: "--DMSans",
+  subsets: ["latin"],
+});
+
+const Montserrat = Montserrat({
+  variable: "--Montserrat",
+  subsets: ["latin"],
+});
+
+const Inter = Inter({
+  variable: "--Inter",
+  subsets: ["latin"],
+});
+
+const Poppins = Poppins({
+  variable: "--Poppins",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "GMT Developers website",
@@ -10,7 +37,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" className={`${Poppins.variable} ${Inter.variable} ${Montserrat.variable} ${DM_Sans.variable} ${Belanosima.variable}`}>
       <head>
         {/*Google tag (gtag.js) */}
         <script async src={`https://www.googletagmanager.com/gtag/js?id=G-DCET6HD4LW`}></script>
